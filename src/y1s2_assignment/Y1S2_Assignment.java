8d96efec88cae8057dd8da380f338289f34e49d7
JC <jcquah2@gmail.com>
2023-05-08 01:35:52 +0800

    ADD NAME

--- src/y1s2_assignment/Y1S2_Assignment.java
+++ src/y1s2_assignment/Y1S2_Assignment.java
@@ -14,7 +14,7 @@ public class Y1S2_Assignment {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello JC");
     }
     
 }
