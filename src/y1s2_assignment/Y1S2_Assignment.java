2db9097b5bc03b4a77ce6b92e08a7fe2cc5f2d38
JC <jcquah2@gmail.com>
2023-05-08 01:22:11 +0800

    Update

---
+++ src/y1s2_assignment/Y1S2_Assignment.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package y1s2_assignment;
+
+/**
+ *
+ * @author Asus
+ */
+public class Y1S2_Assignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
