364d91f0fd5335b194c3bfcf565f964c232fd6fc
JC <jcquah2@gmail.com>
2023-05-10 10:51:44 +0800

    start

--- src/y1s2_assignment/User.java
+++ src/y1s2_assignment/User.java
@@ -4,10 +4,42 @@
  */
 package y1s2_assignment;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Asus
  */
 public class User {
+    String Name;
+    String Username;
+    String EmailAddress;
+    String ContactNumber;
+    String Birthday;
+    int Age;
+    String Address;
+    String Gender;
+    int NumberOfFriends;
+    ArrayList <String> Hobbies=new ArrayList<>();
+    ArrayList <String> Jobs=new ArrayList<>();
+    
+    public User( String Username, String EmailAddress,String ContactNumber){
+    }
+
+    public User(String Name, String Username, String EmailAddress, String ContactNumber, String Birthday, int Age, String Address, String Gender, int NumberOfFriends) {
+        this.Name = Name;
+        this.Username = Username;
+        this.EmailAddress = EmailAddress;
+        this.ContactNumber = ContactNumber;
+        this.Birthday = Birthday;
+        this.Age = Age;
+        this.Address = Address;
+        this.Gender = Gender;
+        this.NumberOfFriends = NumberOfFriends;
+    }
+    
+    
+    
+    
     
 }
